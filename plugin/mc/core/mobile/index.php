b2c07f876652009a39cd45a6f9d46bae312913ba
mzdz <liuqingshan@yeah.net>
2018-03-27 08:56:27 +0800

    fixed

--- plugin/mc/core/mobile/index.php
+++ plugin/mc/core/mobile/index.php
@@ -7,6 +7,17 @@ class Index_EweiShopV2Page extends PluginMobilePage
 		global $_GPC;
 		include $this->template();
 	}
+
+       public function payqrcode() {
+		global $_W;
+		global $_GPC;
+		$openid = $_W['openid'];
+		$id = intval($_GPC['id']);
+		$url = mobileUrl('order/new', array('id' => 213, 'mid' => 97), true);
+		$qrcode = m('qrcode')->createQrcode($url);
+                print_r($_W);
+		include $this->template();
+       }
 }
 
 
